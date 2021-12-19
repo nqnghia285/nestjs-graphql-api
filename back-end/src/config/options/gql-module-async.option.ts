0ff8b3c4eee792ac55f98c66df35b296f0e049f4
nqnghia285 <nqnghia285@gmail.com>
2021-12-19 19:18:02 +0700

    make it better

--- back-end/src/config/options/gql-module-async.option.ts
+++ back-end/src/config/options/gql-module-async.option.ts
@@ -40,7 +40,7 @@ export const gqlModuleAsyncOptions: GqlModuleAsyncOptions = {
          return res
       },
       plugins: [
-         config.system.node_env !== NodeEnv.PRODUCTION
+         config.system.node_env !== NodeEnv.PROVISION
             ? ApolloServerPluginLandingPageGraphQLPlayground()
             : ApolloServerPluginLandingPageDisabled(),
          HandleResolverParams,
